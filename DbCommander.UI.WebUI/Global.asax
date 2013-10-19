0eede79d18435ba151fab0fb2829eeb7eaf88070
Vyacheslav <evangi.studio@hotmail.com>
2013-10-19 23:03:23 +0400

    Initial Empty Project

---
+++ DbCommander.UI.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DbCommander.UI.WebUI.Global" Language="C#" %>
---
+++ DbCommander.UI.WebUI/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Http;
+
+namespace DbCommander.UI.WebUI
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Код, выполняемый при запуске приложения
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);     
+       
+            Bootstrapper.Initialise();
+        }
+    }
+}
